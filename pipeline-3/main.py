a8177060cd120d62999f243cd1016946c7180bbf
pham-k <pham.k@protonmail.com>
2022-05-05 16:52:15 +0700

    Fix typo

--- pipeline-3/main.py
+++ pipeline-3/main.py
@@ -24,7 +24,7 @@ def INFO1001_2021_YRD_1_DUOC_2021_LAN1():
         mon='Thống kê y học [2 TC]', lop='DUOC-2021', 
         lop_hp='211TKY_01001201', lan_thi=obj['lan_thi'],
         ngay_thi='14/04/2022',
-        ngay='      ', thang='04', nam='2022'
+        ngay='      ', thang='05', nam='2022'
     )
 
 def INFO1001_2021_YRD_1_Y_2021_LAN1():
@@ -56,7 +56,7 @@ def INFO1001_2020_YRD_1_RHM_2020_LAN2():
     data = rh.read_data(obj['input'], obj['lan_thi'])
     wh.write_bang_diem_psc_export_INFO1001(
         df=data[0], summary=data[1], path=obj['output'],
-        footer='INFO1001-2021-YRD-1',
+        footer='INFO1001-2020-YRD-1',
         mon='Thống kê y học [2 TC]', lop='RHM-2020', 
         lop_hp='', lan_thi=obj['lan_thi'],
         ngay_thi='14/04/2022',
